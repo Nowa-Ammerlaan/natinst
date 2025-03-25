ced07f95bf02b28b72181a31e81ae659891c42d5
Nowa Ammerlaan <nowa@gentoo.org>
2025-03-25 11:46:56 +0100

    workflows/*: don't use sudo
    Signed-off-by: Nowa Ammerlaan <nowa@gentoo.org>

--- scripts/setup-master-gentoo.sh
+++ scripts/setup-master-gentoo.sh
@@ -3,9 +3,9 @@
 #
 # Fetch and setup the latest ::gentoo and ::guru
 
-sudo mkdir -p /var/db/repos/gentoo /var/db/repos/guru /etc/portage /var/cache/distfiles
-wget -qO - "https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz" | sudo tar xz -C /var/db/repos/gentoo --strip-components=1
-wget -qO - "https://github.com/gentoo-mirror/guru/archive/master.tar.gz" | sudo tar xz -C /var/db/repos/guru --strip-components=1
-sudo wget "https://www.gentoo.org/dtd/metadata.dtd" -O /var/cache/distfiles/metadata.dtd
-sudo cp scripts/stub-repos.conf /etc/portage/repos.conf
-sudo ln -s /var/db/repos/gentoo/profiles/default/linux/amd64/17.1 /etc/portage/make.profile
+mkdir -p /var/db/repos/gentoo /var/db/repos/guru /etc/portage /var/cache/distfiles
+wget -qO - "https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz" | tar xz -C /var/db/repos/gentoo --strip-components=1
+wget -qO - "https://github.com/gentoo-mirror/guru/archive/master.tar.gz" | tar xz -C /var/db/repos/guru --strip-components=1
+wget "https://www.gentoo.org/dtd/metadata.dtd" -O /var/cache/distfiles/metadata.dtd
+cp scripts/stub-repos.conf /etc/portage/repos.conf
+ln -s /var/db/repos/gentoo/profiles/default/linux/amd64/17.1 /etc/portage/make.profile
