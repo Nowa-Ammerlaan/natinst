b4e41b9a053a9c0c39484817e0741eb7e0fd44db
Nowa Ammerlaan <nowa@gentoo.org>
2025-03-25 10:19:12 +0100

    workflows/*: also setup guru repo
    Signed-off-by: Nowa Ammerlaan <nowa@gentoo.org>

--- scripts/setup-master-gentoo.sh
+++ scripts/setup-master-gentoo.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/env bash
 # Maintainer: Nowa Ammerlaan <nowa@gentoo.org>
 #
-# Fetch and setup the latest ::gentoo
+# Fetch and setup the latest ::gentoo and ::guru
 
-sudo mkdir -p /var/db/repos/gentoo /etc/portage /var/cache/distfiles
+sudo mkdir -p /var/db/repos/gentoo /var/db/repos/guru /etc/portage /var/cache/distfiles
 wget -qO - "https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz" | sudo tar xz -C /var/db/repos/gentoo --strip-components=1
+wget -qO - "https://github.com/gentoo-mirror/guru/archive/master.tar.gz" | sudo tar xz -C /var/db/repos/guru --strip-components=1
 sudo wget "https://www.gentoo.org/dtd/metadata.dtd" -O /var/cache/distfiles/metadata.dtd
 sudo wget "https://gitweb.gentoo.org/proj/portage.git/plain/cnf/repos.conf" -O /etc/portage/repos.conf
+cat <<-EOF >> /etc/portage/repos.conf
+[guru]
+location = /var/db/repos/guru
+sync-type = git
+sync-uri = https://github.com/gentoo-mirror/guru.git
+sync-depth = 1
+EOF
 sudo ln -s /var/db/repos/gentoo/profiles/default/linux/amd64/17.1 /etc/portage/make.profile
